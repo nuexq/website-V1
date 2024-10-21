945df630aef3e1d4fb43c7a40aa10ffc24b6311a
No0ne003 <hi.yassir.op@gmail.com>
2024-10-21 15:56:02 +0100

    feat: making projects page better and adding loading to it

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 import Container from "@/components/Container";
+import ProjectCard from "@/components/ProjectCard";
 import Title from "@/components/Title";
-import { ArrowRight } from "lucide-react";
 
 export default function Projects() {
 	return (
@@ -8,22 +8,10 @@ export default function Projects() {
 			<div>
 				<Title title="Projects" />
 			</div>
-			<div className="project-card">
-				<div>
-          <h3>Portfolio</h3>
-          <p>this is my portfolio</p>
-          <div className="underline underline-offset-4 decoration-dotted hover:no-underline transition-all flex justify-end items-center gap-2 cursor-pointer absolute bottom-0 right-0 mr-4 mb-4">see in github <span><ArrowRight size={15} /></span> </div>
-        </div>
-				<div>
-          <h3>React Project</h3>
-          <p>A Collection of small react projects</p>
-          <div className="underline underline-offset-4 decoration-dotted hover:no-underline transition-all flex justify-end items-center gap-2 cursor-pointer absolute bottom-0 right-0 mr-4 mb-4">see in github <span><ArrowRight size={15} /></span> </div>
-        </div>
-				<div>
-          <h3>Simple Blog</h3>
-          <p>a simple Blog build by next.js</p>
-          <div className="underline underline-offset-4 decoration-dotted hover:no-underline transition-all flex justify-end items-center gap-2 cursor-pointer absolute bottom-0 right-0 mr-4 mb-4">see in github <span><ArrowRight size={15} /></span> </div>
-        </div>
+			<div className="flex flex-col w-full gap-4 my-4">
+        <ProjectCard name="Portfolio" desc="This is my Portfolio build in next.js" />
+        <ProjectCard name="React Projects" desc="A collection of React small Projects" />
+        <ProjectCard name="Simple Blog" desc="A collection of React small Projects and no wone kada dakdjf dakd" />
 			</div>{" "}
 		</Container>
 	);
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,18 @@
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+export default function ProjectCard({
+  name,
+  desc,
+}: { name: string; desc: string }) {
+  return (
+    <div className="flex gap-5 sm:gap-24 justify-between items-center bg-neutral-300/60 px-4 py-3 w-full">
+      <div className="flex flex-col gap-4 max-w-md">
+        <h3 className="text-2xl font-geistMono">{name}</h3>
+        <p className="max-sm:text-sm">{desc}</p>
+      </div>
+      <div className="cursor-pointer self-end">
+        <GitHubLogoIcon />
+      </div>
+    </div>
+  );
+}
