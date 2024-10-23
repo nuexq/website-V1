6c7f34e8d5afd20a7267dfa3682d492a85aad78d
No0ne003 <hi.yassir.op@gmail.com>
2024-10-23 19:27:33 +0100

    update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import { siteConfig } from "@/config/site";
 import Link from "next/link";
 
 export default function Home() {
-	return (
-		<Container>
-			<div>
-				<Title title="No0ne" />
-			</div>
-			{/*
+  return (
+    <Container>
+      <div>
+        <Title title="No0ne" />
+      </div>
+      {/*
             <div className="info-card">
               <div>16 years old</div>
               <div>Programmer</div>
@@ -17,51 +17,50 @@ export default function Home() {
               <div>Calistanics</div>
             </div>
             */}
-			<div className="leading-relaxed">
-				<p>
-					Hello I&apos;m {siteConfig.name} or No0ne003 a Front-End developer
-					from morocco, I start with html on{" "}
-					<span className="font-bold">08/26/2023</span> and I&apos;m currently
-					learning{" "}
-					<span className="bg-neutral-300/60 font-bold px-1">
-						C/C++, algorithms & data structers and low level things
-					</span>
-					. I learned backend basics but I need practicing more. maybe in{" "}
-					<span className="font-bold">2025</span>.
-				</p>
-				<br />
-				<p>
-					I&apos;m good at{" "}
-					<span className="bg-neutral-300/60 font-bold">
-						JS/TS, React/Next.js
-					</span>{" "}
-					and ovecourse{" "}
-					<span className="bg-neutral-300/60 font-bold">HTML and CSS</span>{" "}
-					<span className="text-sm">(tailwind btw)</span>. I learned
-					basics of backend in js but I still need practice.
-				</p>
-				<br />
-				<p>
-					I love using <span className="font-bold">Linux</span>{" "}
-					<span className="text-lg">(arch btw)</span> with Hyprland Window
-					Manager, Linux give me what I need as a programmer and it work very
-					well.
-					<br />I also sometimes Game on linux a simple games like stardew
-					valley, hollow knight, and I swear to god I play roblox just with
-					friends for laughing.
-					<br />
-					I&apos;m also a <span className="font-bold">kick boxer</span> for like
-					a 1 year now and I love Calistanics.
-				</p>
-				<br />
-				You can see random stuff about me in{" "}
-				<Link className="underline underline-offset-2" href="/random">
-					here
-				</Link>
-				<br />
-				<br />
-				this is a tested portfolio I make it in 1 hour when the wifi is off
-			</div>
-		</Container>
-	);
+      <div className="leading-relaxed">
+        <p>
+          Hello I&apos;m {siteConfig.name} or No0ne003 a Front-End developer
+          from morocco, I start with html on{" "}
+          <span className="font-bold">08/26/2023</span> and I&apos;m currently
+          learning{" "}
+          <span className="bg-neutral-300/60 font-bold px-1">
+            C/C++, algorithms & data structers and low level things
+          </span>
+          .
+        </p>
+        <br />
+        <p>
+          I&apos;m good at{" "}
+          <span className="bg-neutral-300/60 font-bold">
+            JS/TS, React/Next.js
+          </span>{" "}
+          and ovecourse{" "}
+          <span className="bg-neutral-300/60 font-bold">HTML and CSS</span>{" "}
+          <span className="text-sm">(tailwind btw)</span>. I learned basics of
+          backend in js but I still need practice.
+        </p>
+        <br />
+        <p>
+          I love using <span className="font-bold">Linux</span>{" "}
+          <span className="text-lg">(arch btw)</span> with Hyprland Window
+          Manager, Linux give me what I need as a programmer and it work very
+          well.
+          <br />I also sometimes Game on linux a simple games like stardew
+          valley, hollow knight, and I swear to god I play roblox just with
+          friends for fun.
+          <br />
+          I&apos;m also a <span className="font-bold">kick boxer</span> for like
+          a 1 year now and I love Calistanics.
+        </p>
+        <br />
+        You can see random stuff about me in{" "}
+        <Link className="underline underline-offset-2" href="/random">
+          here
+        </Link>
+        <br />
+        <br />
+        this is a tested portfolio I make it in 1 hour when the wifi is off.
+      </div>
+    </Container>
+  );
 }
