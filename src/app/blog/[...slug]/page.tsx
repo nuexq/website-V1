6c3e6cb87d27a9e856698facf8d3ee7fdf73f566
No0ne003 <hi.yassir.op@gmail.com>
2024-10-27 21:25:19 +0100

    refactor: remove blog post graph

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -40,26 +40,6 @@ export async function generateMetadata({
 		title: `${post.title} | No0ne`,
 		description: post.description,
 		authors: { name: siteConfig.author },
-		openGraph: {
-			title: post.title,
-			description: post.description,
-			type: "article",
-			url: post.slug,
-			images: [
-				{
-					url: `/api/og?${ogSearchParams.toString()}`,
-					width: 1200,
-					height: 630,
-					alt: post.title,
-				},
-			],
-		},
-		twitter: {
-			card: "summary_large_image",
-			title: post.title,
-			description: post.description,
-			images: [`/api/og?${ogSearchParams.toString()}`],
-		},
 	};
 }
 
