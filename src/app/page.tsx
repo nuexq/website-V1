6d692f00fbbf66e46d8885a60773e4786716ddc6
No0ne003 <hi.yassir.op@gmail.com>
2024-11-14 11:13:09 +0100

    feat: changes some description + adding catppuccin webring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
 			</div>
 			<div className="prose">
 				<p>
-					16-year-old highschool student from the Philippines. I’m currently{" "}
-					<span className="highlight">passion for learning</span> with{" "}
+					16-year-old highschool student from the morocco. with a{" "}
+					<span className="highlight">passion for learning</span> and{" "}
 					<span className="highlight">overthinking</span> issues. Into{" "}
 					<u>programming</u>, <u>gaming</u>, and <u>reading</u>, and currently
 					learning <u>C</u>. and I kickbox.
@@ -30,33 +30,31 @@ export default function Home() {
 					some <u>Python</u> and <u>Lua</u>.
 				</p>
 				<p>
-					Working with{" "}
-					<span className="highlight">React, Next.js and Tailwind</span> for
-					styling. Also familiar with <u>Node.js/Bun</u> and <u>MongoDB</u>.
+					Working with <span className="highlight">React, Next.js</span>. Also
+					familiar with Node.js/Bun and Express, MongoDB and mongoose.
 				</p>
 				<h3>Tools</h3>
 				<p>
-					Neovim as my primary editor. Arch Linux. Hardware:{" "}
+					Neovim as my primary editor. Arch Linux. and a{" "}
 					<u>Lenovo ThinkPad T460</u> with an Intel i5-6300U.
 				</p>
 				<h3>Extra</h3>
 				<p>
-					I enjoy anime, TV, movies, and gaming. My favorite show has to be{" "}
-					<u>Breaking Bad</u>, but lately, I’ve really enjoyed{" "}
+					I enjoy watching anime, TV shows, movies, and gaming. My favorite show
+					has to be <u>Breaking Bad</u>, but lately, I’ve really enjoyed{" "}
 					<u>Hajime no Ippo</u>, and I’m currently watching <u>DanDaDan</u>.
-					I’ll keep posting everything I watch or play each year on my{" "}
-					<Link href="/writing">writing</Link> page if you’re curious.
+					maybe I&apos;ll post what I watch or play this year on my{" "}
+					<Link href="/writing">writing</Link> page.
 				</p>
 				<p>
 					As for gaming, I’m into story-driven and challenging games. Favorites
 					include <u>Dark Souls 1</u> and <u>Silent Hill 2</u>,{" "}
-					<u>Stardew valley</u>, <u>Far Cry 3</u>, and lately, I’ve been diving
-					into <u>Portal 2</u>.
+					<u>Stardew valley</u>, <u>Far Cry 3</u>.{" "}
 				</p>
 				<p>
 					Let’s connect: <Link href="https://github.com/No0ne003">GitHub</Link>,{" "}
-					<Link href="https://www.twitter.com/No0ne003">Twitter</Link>, or
-					Discord at No0ne.003.
+					<Link href="https://www.twitter.com/No0ne003">Twitter</Link>,
+					Discord at No0ne.003. <Link href='https://ctp-webr.ing/no0ne/previous'>◄ </Link><Link href='https://ctp-webr.ing/'>webring</Link><Link href='https://ctp-webr.ing/no0ne/next'> ►</Link>
 				</p>
 				<hr className="border-border my-4" />
 				<p className="text-sm">Thanks for stopping by!</p>
