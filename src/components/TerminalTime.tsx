f24b7e11155dbf8d822cd5df501ac39d2905f49a
No0ne003 <hi.yassir.op@gmail.com>
2024-11-17 10:08:35 +0100

    style: TerminalTime text color

--- src/components/TerminalTime.tsx
+++ src/components/TerminalTime.tsx
@@ -22,5 +22,5 @@ export const TerminalTime = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-  return <div className="text-xs text-muted-foreground font-departureMono">{time}</div>;
+  return <div className="text-xs font-departureMono">{time}</div>;
 };
