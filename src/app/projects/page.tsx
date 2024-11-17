b2255ebff10b1572fc96e973a171073cdab8cd52
No0ne003 <hi.yassir.op@gmail.com>
2024-11-17 15:17:03 +0100

    feat: project stack to string[]

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,59 +8,57 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-	title: "[P}rojects",
-	description: "Collection of projects",
+  title: "[P}rojects",
+  description: "Collection of projects",
 };
 
 export default function Projects() {
-	return (
-		<Container>
-			<div className="flex-1 space-y-2 mb-6">
-				<Title title="Projects" />
-				<p className="text-muted-foreground text-sm">
-					You can see all my project in my{" "}
-					<Link
-						href={siteConfig.links.github}
-						className={cn(
-							buttonVariants({ variant: "link" }),
-							"text-foreground",
-						)}
-						target="_blank"
-					>
-						github
-					</Link>
-				</p>
-			</div>
-			<hr className="border-border mb-3" />
-			<div className="w-full flex flex-col gap-3">
-				<div className="flex flex-col gap-6">
-					<ProjectCard
-						href="react-project"
-						name="React Projects"
-						desc="A collection of small React Projects."
-						stack="React"
-            website="no0ne003.github.io/React-Projects/"
-					/>
-					<ProjectCard
-						href="simpleBlog"
-						name="Simple Blog"
-						desc="A simple blog build with nextJs and Velite."
-						stack="NextJs, Velite"
-            website="simpleblog003.vercel.app/"
-					/>
-					<ProjectCard
-						href="dotfiles"
-						name="My dotfiles"
-						desc="My Arch linux hyprland dotfiles, also my neovim config."
-					/>
-					<ProjectCard
-						href="website"
-						name="Website"
-						desc="The first version of my personal website, build in next.js."
-						stack="NextJs, Velite"
-					/>
-				</div>
-			</div>{" "}
-		</Container>
-	);
+  return (
+    <Container>
+      <div className="flex-1 space-y-2 mb-6">
+        <Title title="Projects" />
+        <p className="text-muted-foreground text-sm">
+          You can see all my project in my{" "}
+          <Link
+            href={siteConfig.links.github}
+            className={cn(
+              buttonVariants({ variant: "link" }),
+              "text-foreground",
+            )}
+            target="_blank"
+          >
+            github
+          </Link>
+        </p>
+      </div>
+      <hr className="border-border mb-3" />
+      <div className="flex flex-col gap-4">
+        <ProjectCard
+          href="react-project"
+          name="React Projects"
+          desc="A collection of small React Projects."
+          stack={["React"]}
+          website="no0ne003.github.io/React-Projects/"
+        />
+        <ProjectCard
+          href="simpleBlog"
+          name="Simple Blog"
+          desc="A simple blog build with nextJs and Velite."
+          stack={["NextJs", "Velite"]}
+          website="simpleblog003.vercel.app/"
+        />
+        <ProjectCard
+          href="dotfiles"
+          name="My dotfiles"
+          desc="My Arch linux hyprland dotfiles, also my neovim config."
+        />
+        <ProjectCard
+          href="website"
+          name="Website"
+          desc="The first version of my personal website, build in next.js."
+          stack={["NextJs", "Velite"]}
+        />
+      </div>
+    </Container>
+  );
 }
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,36 +3,47 @@ import { siteConfig } from "@/config/site";
 import { Globe } from "lucide-react";
 
 export default function ProjectCard({
-	name,
-	desc,
-	href,
-	website,
-	stack,
+  name,
+  desc,
+  href,
+  website,
+  stack,
 }: {
-	name: string;
-	desc: string;
-	href: string;
-	stack?: string;
-	website?: string;
+  name: string;
+  desc: string;
+  href: string;
+  stack?: string[];
+  website?: string;
 }) {
-	return (
-		<div className="flex flex-col gap-1 p-2">
-			<div className="w-full flex justify-between items-center font-geistMono">
-				<h2 className="flex items-center gap-2 text-lg underline decoration-dotted decoration-1 underline-offset-4 decoration-foreground/20 hover:decoration-foreground">
-					<Link href={`${siteConfig.links.github}/${href}`} target="_blank">
-						{name}
-					</Link>
-					{website ? (
-						<Link href={`https://${website}`} target="_blank">
-							<Globe className="size-3 text-muted-foreground" />
-						</Link>
-					) : null}
-				</h2>
-				<p className="text-xs highlight before:content-[''] after:content-['']">
-					{stack ? `[${stack}]` : null}
-				</p>
-			</div>
-			<p className="text-sm text-muted-foreground">{desc}</p>
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-1 p-2">
+      <div className="w-full flex justify-between items-center font-geistMono">
+        <h2 className="flex items-center gap-2 text-lg underline decoration-dotted decoration-1 underline-offset-4 decoration-foreground/20 hover:decoration-foreground">
+          <Link href={`${siteConfig.links.github}/${href}`} target="_blank">
+            {name}
+          </Link>
+          {website ? (
+            <Link href={`https://${website}`} target="_blank">
+              <Globe className="size-3 text-muted-foreground" />
+            </Link>
+          ) : null}
+        </h2>
+        <p className="text-xs highlight before:content-[''] after:content-['']">
+          {stack ? (
+            <span>
+              [
+              {stack.map((item, index) => [
+                <span key={index}>
+                  {item}
+                  {index !== stack.length - 1 ? "/" : ""}
+                </span>,
+              ])}
+              ]
+            </span>
+          ) : null}
+        </p>
+      </div>
+      <p className="text-sm text-muted-foreground">{desc}</p>
+    </div>
+  );
 }
