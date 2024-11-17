51a302284cd7b2734a1ba2623033692858af3d1e
No0ne003 <hi.yassir.op@gmail.com>
2024-11-17 15:29:10 +0100

    feat: shadcn skeleton to all loadings

--- src/app/writing/[...slug]/loading.tsx
+++ src/app/writing/[...slug]/loading.tsx
@@ -1,10 +1,11 @@
 import Container from "@/components/Container";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
 	return (
 		<Container>
-			<div className="w-52 h-10 bg-neutral-300 animate-pulse mb-6" />
-			<div className="w-80 sm:w-96 h-56 bg-neutral-300 animate-pulse" />
+			<Skeleton className="w-52 h-10 bg-neutral-300 mb-6" />
+			<Skeleton className="w-80 sm:w-96 h-56 bg-neutral-300" />
 		</Container>
 	);
 }
