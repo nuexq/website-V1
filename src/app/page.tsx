2ba6c6c8053ad1dc63c2971338bdc8f4403860e2
No0ne003 <hi.yassir.op@gmail.com>
2024-11-17 10:18:11 +0100

    refactor: grammer issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,40 +16,41 @@ export default function Home() {
 			</div>
 			<div className="prose">
 				<p>
-					16-year-old highschool student from the morocco. with a{" "}
+					16-year-old high school student from Morocco. with a{" "}
 					<span className="highlight">passion for learning</span> and{" "}
 					<span className="highlight">overthinking</span> issues. Into{" "}
 					<u>programming</u>, <u>gaming</u>, and <u>reading</u>, and I kickbox.
 				</p>
 				<h3>Tech</h3>
 				<p>
-					Web-focused but exploring{" "}
-					<u>algorithms, data structures, and low-level programming</u>. I know:{" "}
-					<span className="highlight">JS/TS</span>, and currently practicing{" "}
-					<u>C</u>, plus I know kinda <u>Python</u> and <u>Lua</u>.
+					I&apos;m web-focused but also exploring{" "}
+					<u>algorithms, data structures, and low-level programming.</u>. I
+					know: <span className="highlight">JS/TS</span>, and currently
+					practicing <u>C</u>,I also have some familiarity with <u>Python</u>{" "}
+					and <u>Lua</u>.
 				</p>
 				<p>
-					Working with <span className="highlight">React, Next.js</span>. Also
-					familiar with Node.js/Bun, Express and MongoDB/mongoose.
+					I work with <span className="highlight">React, Next.js</span> and am
+					also familiar with Node.js/Bun, Express, and MongoDB/mongoose.
 				</p>
 				<h3>Tools</h3>
 				<p>
-					Neovim as my primary editor. Arch Linux. and a{" "}
-					<u>Lenovo ThinkPad T460</u> with an Intel i5-6300U.
+					Neovim is my primary editor, and I use Arch Linux on a{" "}
+					<u>Lenovo ThinkPad T460</u> with an Intel i5-6300U.{" "}
 				</p>
 				<h3>Extra</h3>
 				<p>
-					I enjoy watching anime, TV shows, movies, and gaming. My favorite show
-					has to be <u>Breaking Bad</u>, but lately, I’ve really enjoyed{" "}
+					I enjoy watching anime, TV shows, movies, and gaming, My favorite show
+					is <u>Breaking Bad</u>, but lately, I’ve really enjoyed{" "}
 					<u>Hajime no Ippo</u>, and I’m currently watching <u>DanDaDan</u>.
-					maybe I&apos;ll post what I watch or play this year on my{" "}
+					Maybe I’ll post what I watch or play this year on my{" "}
 					<Link href="/writing">writing</Link> page.
 				</p>
 				<p>
-					As for gaming, I’m into story-driven and challenging games. Favorites
-					include <u>Dark Souls 1</u> and <u>Silent Hill 2</u>,{" "}
-					<u>Stardew valley</u>, <u>Far Cry 3</u>. I love calisthenics and I
-					want to improve in it (still 8 sec at L-sit).
+					As for gaming, I’m into story-driven and challenging games. My
+					favorites include <u>Dark Souls 1</u>, <u>Silent Hill 2</u>,{" "}
+					<u>Stardew Valley</u> and <u>Far Cry 3</u>. I love calisthenics, and I
+					want to improve at it (currently holding an 8-second L-sit).
 				</p>
 				<p>
 					Let’s connect:{" "}
@@ -60,7 +61,7 @@ export default function Home() {
 					<Link href="https://www.twitter.com/No0ne003" target="_blank">
 						Twitter
 					</Link>
-					, Discord at No0ne.003.{" "}
+					, Discord (No0ne.003).{" "}
 					<Link href="https://ctp-webr.ing/no0ne/previous">◄ </Link>
 					<Link href="https://ctp-webr.ing/">webring</Link>
 					<Link href="https://ctp-webr.ing/no0ne/next"> ►</Link>
