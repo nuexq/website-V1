04e3bc8fe0f6029d27375ab378b0c45452f81c96
No0ne003 <hi.yassir.op@gmail.com>
2024-11-17 12:34:51 +0100

    style: PostCard hover underline color

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -2,29 +2,29 @@ import Link from "next/link";
 import { formateDate } from "@/lib/utils";
 
 interface PostItemProps {
-	slug: string;
-	title: string;
-	description?: string;
-	date: string;
+  slug: string;
+  title: string;
+  description?: string;
+  date: string;
 }
 
 const PostCard = ({ slug, title, description, date }: PostItemProps) => {
-	return (
-		<article className="flex flex-col gap-1 p-2">
-			<div className="w-full flex justify-between items-center font-geistMono">
-				<h2 className="text-lg underline decoration-dotted decoration-1 underline-offset-4 decoration-foreground/20 hover:decoration-blue-600">
-					<Link href={"/" + slug}>{title}</Link>
-				</h2>
-				<dl>
-					<dt className="sr-only">Published On</dt>
-					<dd className="text-xs text-muted-foreground flex items-center gap-1">
-						<time dateTime={date}>{formateDate(date)}</time>
-					</dd>
-				</dl>
-			</div>
-			<p className="text-sm text-muted-foreground">{description}</p>
-		</article>
-	);
+  return (
+    <article className="flex flex-col gap-1 p-2">
+      <div className="w-full flex justify-between items-center font-geistMono">
+        <h2 className="text-lg underline decoration-dotted decoration-1 underline-offset-4 decoration-foreground/20 hover:decoration-foreground">
+          <Link href={"/" + slug}>{title}</Link>
+        </h2>
+        <dl>
+          <dt className="sr-only">Published On</dt>
+          <dd className="text-xs text-muted-foreground flex items-center gap-1">
+            <time dateTime={date}>{formateDate(date)}</time>
+          </dd>
+        </dl>
+      </div>
+      <p className="text-sm text-muted-foreground">{description}</p>
+    </article>
+  );
 };
 
 export default PostCard;
