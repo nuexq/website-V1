9f69909e42d8fbae960ca3a38a3e7f2937655e2d
No0ne003 <hi.yassir.op@gmail.com>
2024-11-19 12:45:13 +0100

    fix: tailwind for ./src/features

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -4,37 +4,37 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export const navLinks = [
-	{ href: "/", label: "Home" },
-	{ href: "/projects", label: "Projects" },
-	{ href: "/writing", label: "Writing" },
+  { href: "/", label: "Home" },
+  { href: "/projects", label: "Projects" },
+  { href: "/writing", label: "Writing" },
 ];
 
 export const Nav = () => {
-	const pathname = usePathname();
-	const pathnameArray = pathname.split("/");
+  const pathname = usePathname();
+  const pathnameArray = pathname.split("/");
 
-	return (
-		<nav className="nav md:border-r border-border">
-			<div className="flex flex-col">
-				{navLinks.map((link) => (
-					<Link
-						key={link.href}
-						href={link.href}
+  return (
+    <nav className="nav md:border-r border-border">
+      <div className="flex flex-col">
+        {navLinks.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
             className="px-3 py-2 sm:hover:bg-border/60 font-geistMono w-44 cursor-pointer border-b border-border last:border-none"
-					>
-						<span
-							className={
-								pathname === link.href || `/${pathnameArray[1]}` === link.href
-									? "font-[900]"
-									: ""
-							}
-						>
-							[{link.label.slice(0, 1)}]
-						</span>
-						<span>{link.label.slice(1, link.label.length)}</span>
-					</Link>
-				))}
-			</div>
-		</nav>
-	);
+          >
+            <span
+              className={
+                pathname === link.href || `/${pathnameArray[1]}` === link.href
+                  ? "font-[900]"
+                  : ""
+              }
+            >
+              [{link.label.slice(0, 1)}]
+            </span>
+            <span>{link.label.slice(1, link.label.length)}</span>
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
 };
