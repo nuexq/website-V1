cc6bd7a34f3d33d689d2439088396b2d5be41e66
No0ne003 <hi.yassir.op@gmail.com>
2025-01-01 14:16:56 +0100

    style: responsive tables in mobile

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -11,13 +11,13 @@ interface PostItemProps {
 const PostCard = ({ slug, title, description, date }: PostItemProps) => {
   return (
     <article className="flex flex-col gap-1 p-2">
-      <div className="w-full flex justify-between items-center font-geistMono">
+      <div className="w-full flex justify-between items-start gap-2 font-geistMono">
         <h2 className="text-lg dotted-underline">
           <Link href={"/" + slug}>{title}</Link>
         </h2>
         <dl>
           <dt className="sr-only">Published On</dt>
-          <dd className="text-xs text-muted-foreground flex items-center gap-1">
+          <dd className="text-xs text-muted-foreground flex gap-1">
             <time dateTime={date}>{formateDate(date)}</time>
           </dd>
         </dl>
