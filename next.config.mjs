017902440637c0b2f8e42578a1f6200ac3b367a6
nuexq <hi.yassir.op@gmail.com>
2025-02-17 22:23:16 +0100

    feat: no static, og images breaks

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,13 @@
 import { build } from "velite";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	output: "export", // Enables full static export
-	trailingSlash: true, // Ensures /about/ instead of /about (useful for static hosting)
+export default {
+	// othor next config here...
 	webpack: (config) => {
 		config.plugins.push(new VeliteWebpackPlugin());
 		return config;
 	},
 };
-
-export default nextConfig;
 
 class VeliteWebpackPlugin {
 	static started = false;
@@ -18,14 +15,15 @@ class VeliteWebpackPlugin {
 		this.options = options;
 	}
 	apply(/** @type {import('webpack').Compiler} */ compiler) {
+		// executed three times in nextjs !!!
+		// twice for the server (nodejs / edge runtime) and once for the client
 		compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
 			if (VeliteWebpackPlugin.started) return;
 			VeliteWebpackPlugin.started = true;
 			const dev = compiler.options.mode === "development";
 			this.options.watch = this.options.watch ?? dev;
 			this.options.clean = this.options.clean ?? !dev;
-			await build(this.options); // Start Velite
+			await build(this.options); // start velite
 		});
 	}
 }
-
