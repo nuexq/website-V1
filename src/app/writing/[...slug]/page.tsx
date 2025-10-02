e813363736d6e637cbbd8e591029d29fee4f092e
nuexq <nuexqq@gmail.com>
2025-10-02 07:41:22 +0100

    remove runtime edge from writing pages

--- src/app/writing/[...slug]/page.tsx
+++ src/app/writing/[...slug]/page.tsx
@@ -16,8 +16,6 @@ interface PostPageProps {
 	};
 }
 
-export const runtime = 'edge';
-
 async function getPostFromParams(params: PostPageProps["params"]) {
 	const slug = params?.slug?.join("/");
 	const post = posts.find((post) => post.slugAsParams === slug);
